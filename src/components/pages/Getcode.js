d7837c19c1649bcde267146d6e8e149d46073502
Mr-Ananymous <64893079+Mr-Ananymous@users.noreply.github.com>
2021-05-12 19:55:27 +0530

    Add files via upload

---
+++ src/components/pages/Getcode.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css'
+import Uploadimg from '../uploadimg'
+
+import Footer from '../Footer'
+function Getcode() {
+    return (
+        <>
+          <Uploadimg />
+          <Footer />            
+        </>
+    )
+}
+
+export default Getcode
